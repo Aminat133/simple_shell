f841bb6fde4a5357afd57010f4aec25809f9e527
Armand75 <isubiza56@gmail.com>
2023-10-18 11:53:57 -0700

    getline correction

--- _getline.c
+++ _getline.c
@@ -1,8 +1,88 @@
 #include "main.h"
+
 /**
- * _getline - reads and stores the input
- * from the keyboard.
- * @line: duble pointer to storage.
+ * allocateMemory - allocates memory for line buffer
+ * @line: double pointer to line buffer
+ * @size: pointer to size of line buffer
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int allocateMemory(char **line, size_t *size)
+{
+	if (line == NULL || size == NULL)
+		return (-1);
+
+	*size = 128;
+	*line = malloc(*size);
+	if (*line == NULL)
+		return (-1);
+
+	return (0);
+}
+
+/**
+ * readCharacter - reads a character from the stream
+ * @stream: pointer to stream to read from
+ * @line: double pointer to line buffer
+ * @bytesRead: pointer to bytes read
+ * @totalBytesRead: pointer to total bytes read
+ * @size: pointer to size of line buffer
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int readCharacter(FILE *stream, char **line, ssize_t *bytesRead,
+		ssize_t *totalBytesRead, size_t *size)
+{
+	int c;
+
+	c = fgetc(stream);
+	if (c == EOF)
+	{
+		if (*bytesRead == 0)
+			return (-1);
+		return (1);
+	}
+	(*line)[*bytesRead] = (char)c;
+	(*bytesRead)++;
+	(*totalBytesRead)++;
+	if (*bytesRead >= (ssize_t)(*size - 1))
+	{
+		if (resizeLineBuffer(line, size) == -1)
+			return (-1);
+	}
+	if (c == '\n')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * resizeLineBuffer - resizes the line buffer
+ * @line: double pointer to line buffer
+ * @size: pointer to size of line buffer
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int resizeLineBuffer(char **line, size_t *size)
+{
+	char *newLine;
+
+	*size *= 2;
+	newLine = realloc(*line, *size);
+	if (newLine == NULL)
+	{
+		free(*line);
+		*line = NULL;
+		return (-1);
+	}
+	*line = newLine;
+
+	return (0);
+}
+
+/**
+ * _getline - reads and stores the input from the keyboard.
+ * @line: double pointer to storage.
  * @size: size of storage.
  * @stream: where to read the input from.
  *
@@ -12,14 +92,13 @@ ssize_t _getline(char **line, size_t *size, FILE *stream)
 {
 	ssize_t bytesRead = 0, totalBytesRead = 0;
 	int c;
-	char *newLine;
 
 	if (line == NULL || size == NULL || stream == NULL)
 		return (-1);
 	if (*line == NULL)
 	{
 		*size = 128;
-		*line = (char *)malloc(*size);
+		*line = malloc(*size);
 		if (*line == NULL)
 			return (-1);
 	}
@@ -36,21 +115,18 @@ ssize_t _getline(char **line, size_t *size, FILE *stream)
 		totalBytesRead++;
 		if (bytesRead >= (ssize_t)(*size - 1))
 		{
-			*size *= 2;
-	newLine = (char *)realloc(*line, *size);
-			if (newLine == NULL)
+			if (resizeLineBuffer(line, size) == -1)
 			{
 				free(*line);
 				*line = NULL;
 				return (-1);
 			}
-			*line = newLine;
 		}
 		if (c == '\n')
 			break;
 	}
 	(*line)[bytesRead] = '\0';
+
 	return (totalBytesRead);
 }
-
 
--- main.h
+++ main.h
@@ -18,6 +18,10 @@ void execute_parent_process(pid_t pid);
 char *_getenv(const char *name);
 ssize_t _getline(char **line, size_t *size, FILE *stream);
 char *_strtok(char *str, const char *delim);
+int allocateMemory(char **line, size_t *size);
+int readCharacter(FILE *stream, char **line, ssize_t *bytesRead,
+		ssize_t *totalBytesRead, size_t *size);
+int resizeLineBuffer(char **line, size_t *size);
 
 #endif
 
